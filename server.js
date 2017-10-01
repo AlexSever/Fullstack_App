9b0705679bc83b0f69ce3acf7edba5954e3aa07e
AlexSever <alexei.miasin@gmail.com>
2017-09-30 20:56:30 -0400

    handling an issue

--- server.js
+++ server.js
@@ -31,12 +31,13 @@ if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
     // like our main.js file, or main.css file!
 
-    app.use(express.static('client/build'));
+    const path = require('path');
+
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     // Express will serve up the index.html file
     // if it doesn't recognize the route
 
-    const path = require('path');
     app.get('*', function(req, res) {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
