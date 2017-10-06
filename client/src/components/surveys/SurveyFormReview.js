cc8bb2b2e28f56dca7f33b3ad98e0d12791fbc44
AlexSever <alexei.miasin@gmail.com>
2017-10-06 12:55:25 -0400

    added a redirect to /surveys after clicking Send Survey button

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
@@ -27,7 +28,7 @@ const SurveyReview = (props) => {
             </button>
             <button
                 className="green btn-flat right white-text"
-                onClick={() => props.submitSurvey(props.formValues)}
+                onClick={() => props.submitSurvey(props.formValues, props.history)}
             >
                 Send Survey
                 <i className="material-icons right">email</i>
@@ -40,4 +41,4 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps, actions)(SurveyReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyReview));
